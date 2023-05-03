ad2f013fcd6d13025f1e7aa296b35d83a283ef33
viniolvs <vinioliveira.s@hotmail.com>
2023-05-03 08:18:29 -0300

    fix: about

--- frontend/src/pages/about/index.jsx
+++ frontend/src/pages/about/index.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import Container from "@mui/material/Container";
 
 export default function About() {
   return (
     <div className="aboutbody">
-      <Container  //organizar container!
-        sx={{ bgcolor: 'var(--cor4)', height: '100vh', borderRadius: 8, width: '135vh' }}
-      ><h1>Sobre nos</h1>
+      <Container //organizar container!
+        sx={{
+          bgcolor: "var(--cor4)",
+          height: "100vh",
+          borderRadius: 8,
+          width: "135vh",
+        }}
+      >
+        <h1>Sobre nos</h1>
         <div>
           <img className="aboutimg" src="https://i.imgur.com/4jsdv2b.png" />
         </div>
