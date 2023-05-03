2d94db25bcd769e9e1f88cb6c4337f0a0a5748fc
viniolvs <vinioliveira.s@hotmail.com>
2023-05-03 09:45:45 -0300

    add: cors

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,15 @@ import { PrismaClient } from "@prisma/client";
 import express from "express";
 import path from "path";
 import url from "url";
+import cors from "cors";
 
 const app = express();
 const prisma = new PrismaClient();
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 app.use("/", express.static(path.join(__dirname, "./../frontend")));
+app.use(cors());
 
 app.get("/playlists", async (req, res) => {
   const playlists = await prisma.playlist.findMany();
