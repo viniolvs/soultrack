71c7e88f77d20f7a083adc27d4273ba9a316fddd
vinicius <viniciusolvs@proton.me>
2023-05-13 20:41:46 -0300

    feat: add playlist and delete playlist

--- backend/index.ts
+++ backend/index.ts
@@ -4,27 +4,51 @@ import express from "express";
 import path from "path";
 import url from "url";
 import cors from "cors";
+import bodyParser from "body-parser";
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 const app = express();
+const prisma = new PrismaClient();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use("/", express.static(path.join(__dirname, "./../frontend")));
 app.use(cors());
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
 
 app.get("/playlists", async (req, res) => {
-  const prisma = new PrismaClient();
   const playlists = await prisma.playlist.findMany();
   res.json(playlists);
 });
+
+app.post("/addPlaylist", async (req, res) => {
+  const playlist = req.body
+  const { name, description, author, tag } = playlist
+  const user_src: string = playlist.src + "?utm_source=generator"
+  const src = user_src.replace('/playlist/', '/embed/playlist/')
+  const result = await prisma.playlist.create({
+    data: {
+      name,
+      description,
+      author,
+      tag,
+      src,
+    },
+  })
+  res.json(playlist)
+})
+
+app.delete("/playlists/:id", async (req, res) => {
+  const id: number = parseInt(req.params.id)
+  const playlist = await prisma.playlist.delete({
+    where: {
+      id,
+    },
+  })
+  res.json(playlist)
+})
 
 const PORT = 3000;
 const server = app.listen(PORT, () => {
